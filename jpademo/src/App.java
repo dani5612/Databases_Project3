47412679d4b94c5796be25cc043dc472aa9345d4
GioEpic123 <giovanni.quevedo5@gmail.com>
2021-12-01 13:44:44 -0800

    Re-Introduced sample code for debugging. Changed persistence.xml and orm.xml accordingly

--- jpademo/src/App.java
+++ jpademo/src/App.java
@@ -183,7 +183,7 @@ public class App {
 
     }
     public static void main(String[] args) throws Exception {
-        // basicDemos();
-        associationDemos();
+        basicDemos();
+        //associationDemos();
     }
 }
