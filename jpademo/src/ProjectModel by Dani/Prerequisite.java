b166a631f42d8bbac64e4bd6339b722fb307762a
dani5612 <lapicero.30@gmail.com>
2021-12-06 15:05:07 -0800

    Update Prerequisite.java

--- jpademo/src/ProjectModel by Dani/Prerequisite.java
+++ jpademo/src/ProjectModel by Dani/Prerequisite.java
@@ -5,15 +5,18 @@ import jakarta.persistence.*;
 @Entity(name = "PREREQUISITES")
 public class Prerequisite{
     //Attributes required
+@Id
     private char minimumGrade;
 
     // Since association is recursive,
     // we need two Course ID FKs
+@Id
     @ManyToOne
     @JoinColumn(name = "FOLLOWUP_ID")
     private Course followupCourse; 
     //Tried specifiying another Many to one with join column here, and Eclipse Persistance HATED that.
     //Leaving it blank for now
+@Id
     @ManyToOne
     @JoinColumn(name = "PREREQ_ID")
     private Course prereqCourse;
